31379d3b25317d0c1738587ffaf46cb1647e5168
Roman Saykin <sc2aru@gmail.com>
2019-11-16 17:56:26 +0300

    created first enemy and animated him (walk, spawning)

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)] [SerializeField] private float _walkSpeed;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * _walkSpeed *Time.deltaTime);
+    }
+}
