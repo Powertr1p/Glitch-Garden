014a36e047ad9809054e902438f23cccc9c3db86
Roman Saykin <sc2aru@gmail.com>
2019-11-17 11:33:52 +0300

    added keyframe methods

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
-    [Range(0f, 5f)] [SerializeField] private float _walkSpeed;
+    [Range(0f, 5f)]
+    private float _currentSpeed = 1.25f;
 
     private void Update()
     {
-        transform.Translate(Vector2.left * _walkSpeed *Time.deltaTime);
+        transform.Translate(Vector2.left * _currentSpeed *Time.deltaTime);
+    }
+
+    public void SetMovementSpeed(float _speed)
+    {
+        _currentSpeed = _speed;
     }
 }
