d2831619c38e2ef35d8305b6c67f9f3e19671822
Roman Saykin <sc2aru@gmail.com>
2019-11-17 11:21:37 +0300

    created lizard spawner

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _lizard;
+    [SerializeField] private float _minSpawnDelay = 1f;
+    [SerializeField] private float _maxSpawnDelay = 5f;
+
+    private bool _spawn = true;
+
+    private IEnumerator Start()
+    {
+        while (_spawn)
+        {
+           yield return new WaitForSeconds(Random.Range(_minSpawnDelay, _maxSpawnDelay));
+           SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(_lizard, transform.position, Quaternion.identity);
+    }
+
+}
