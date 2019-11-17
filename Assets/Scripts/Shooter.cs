f6ca141e533d560bfcc4d9d8caccf009012e6e60
Roman Saykin <sc2aru@gmail.com>
2019-11-17 14:21:46 +0300

    added a gun, added a projectile

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] private GameObject _projectile;
+    [SerializeField] private Transform _gunPosition;
+
+   public void Shoot()
+   {
+        Instantiate(_projectile, _gunPosition.transform.position, transform.rotation);
+   }
+}
