f42ab276e078d91f3f784f954055b9a2fb2e1029
Roman Saykin <sc2aru@gmail.com>
2019-11-19 00:18:39 +0300

    implemented a spawnzone and created logic for instanciate defender on mouse click

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _cactus;
+
+    private void OnMouseDown()
+    {
+        Spawn(GetClickedPosition());
+    }
+
+    private Vector2 GetClickedPosition()
+    {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+
+        return worldPos;
+    }
+
+    private void Spawn(Vector2 spawnPosition)
+    {
+        GameObject newDefender = Instantiate(_cactus, spawnPosition, Quaternion.identity) as GameObject;
+    }
+}
