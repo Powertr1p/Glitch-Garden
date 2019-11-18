ccd230c21a4f94a92bd7c20cec9fa42cffc09e5d
Roman Saykin <sc2aru@gmail.com>
2019-11-18 23:48:46 +0300

    added VFX for attackers death

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Attacker : MonoBehaviour
 {
-    [Range(0f, 5f)]
     private float _currentSpeed = 1.25f;
 
     private void Update()
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] private float _health = 100f;
+    [SerializeField] private GameObject _deathVFX;
 
     public void ApplyDamage(float damage)
     {
         _health -= damage;
 
         if (_health <= 0)
+        {
+            TriggerDeathVFX();
             Destroy(gameObject);
+        }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if(!_deathVFX) { return; }
+        GameObject deathEffects = Instantiate(_deathVFX, transform.position, transform.rotation);
+        Destroy(deathEffects, 1f);
     }
 }
