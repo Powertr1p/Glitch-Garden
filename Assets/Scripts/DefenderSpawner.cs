1e4c977c116e97f5bbbf3a306e1f89993f93f6d9
Powertr1p <sc2aru@gmail.com>
2019-11-19 19:00:04 +0300

    setted up a spawn point of a defender

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -15,12 +15,22 @@ public class DefenderSpawner : MonoBehaviour
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        Vector2 roundedPos = SnapToGrid(worldPos);
 
-        return worldPos;
+        return roundedPos;
+    }
+
+    private Vector2 SnapToGrid(Vector2 rawWorldPos)
+    {
+        float newX = Mathf.RoundToInt(rawWorldPos.x);
+        float newY = Mathf.RoundToInt(rawWorldPos.y);
+
+        return new Vector2(newX, newY);
     }
 
     private void Spawn(Vector2 spawnPosition)
     {
         GameObject newDefender = Instantiate(_cactus, spawnPosition, Quaternion.identity) as GameObject;
+        Debug.Log(spawnPosition);
     }
 }
