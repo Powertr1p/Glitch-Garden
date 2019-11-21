5bf1f337c750517ebc6add6e9a4f1dd910a7924e
Roman Saykin <sc2aru@gmail.com>
2019-11-21 23:56:53 +0300

    added enemy detector and spawner detector to the shooter

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -7,7 +7,43 @@ public class Shooter : MonoBehaviour
     [SerializeField] private GameObject _projectile;
     [SerializeField] private GameObject _gun;
 
-   public void Shoot()
+    private AttackerSpawner _myLaneSpawner;
+
+    private void Start()
+    {
+        SetLaneSpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+            Debug.Log("shoot pew-pew");
+        else
+            Debug.Log("sit and wait");
+    }
+
+    private void SetLaneSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach(AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(Mathf.RoundToInt(spawner.transform.position.y) - transform.position.y) <= Mathf.Epsilon);
+
+            if (isCloseEnough)
+                _myLaneSpawner = spawner;
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (_myLaneSpawner.transform.childCount <= 0)
+            return false;
+        else
+            return true;
+    }
+
+    public void Shoot()
    {
         Instantiate(_projectile, _gun.transform.position, transform.rotation);
    }
