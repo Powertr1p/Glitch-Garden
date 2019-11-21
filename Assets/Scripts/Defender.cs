e35e8833f91698adaba63a9086a0dd663edc8ac9
Powertr1p <sc2aru@gmail.com>
2019-11-21 16:12:01 +0300

    added visual animation and add star methos to trophy

--- Assets/Scripts/Defender.cs
+++ Assets/Scripts/Defender.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 public class Defender : MonoBehaviour
 {
     [SerializeField] private int _starCost = 100;
+
+    public void AddStars(int amount)
+    {
+        FindObjectOfType<ScoreStars>().AddStars(amount);
+    }
+
 }
