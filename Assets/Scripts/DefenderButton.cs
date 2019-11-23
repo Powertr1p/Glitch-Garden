84b0b3c805e6e6aa92e0bc0e64e2f5c6d14ef658
Roman Saykin <sc2aru@gmail.com>
2019-11-23 14:12:05 +0300

    created another new defender

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -9,10 +9,12 @@ public class DefenderButton : MonoBehaviour
     private void OnMouseDown()
     {
         var buttons = FindObjectsOfType<DefenderButton>();
-        foreach(DefenderButton button in buttons)
+
+        foreach (DefenderButton button in buttons)
         {
             button.GetComponent<SpriteRenderer>().color = new Color32(55, 55, 55, 255);
         }
+
         GetComponent<SpriteRenderer>().color = Color.white;
 
         FindObjectOfType<DefenderSpawner>().SetSelectedDefender(_defenderPrefab);
