a26c772cdd3904cce2be72a56dbe51ddb684984f
Roman Saykin <sc2aru@gmail.com>
2019-11-27 23:59:55 +0300

    added game timer and interactive slider

---
+++ Assets/Scripts/GameTimerSlider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimerSlider : MonoBehaviour
+{
+    [Tooltip("Level duration in seconds")]
+    [SerializeField] private float _levelTime = 10f;
+
+    private void Update()
+    {
+        GetComponent<Slider>().value = Time.timeSinceLevelLoad / _levelTime;
+
+        bool timerFinished = (Time.timeSinceLevelLoad >= _levelTime);
+        if (timerFinished)
+            Debug.Log("Timer Finished");
+    }
+}
