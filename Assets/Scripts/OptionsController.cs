c92ce307b7ec142b014db5248cdc8a501221e537
Roman Saykin <sc2aru@gmail.com>
2019-12-02 22:05:15 +0300

    implemented music volume slider and logic for change music volume

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    [SerializeField] private Slider _volumeSlider;
+    [SerializeField] private float _defaultVolume = 0.5f;
+
+    private void Start()
+    {
+        _volumeSlider.value = PlayerPrefsController.GetMasterVolume();
+    }
+
+    private void Update()
+    {
+        var musicPlayer = FindObjectOfType<MusicPlayer>();
+        if (musicPlayer)
+            musicPlayer.SetVolume(_volumeSlider.value);
+        else
+            Debug.LogWarning("No music player found");
+    }
+
+    public void SaveAndExit()
+    {
+        PlayerPrefsController.SetMasterVolume(_volumeSlider.value);
+        FindObjectOfType<LevelLoader>().LoadMainMenu();
+    }
+
+    public void SetToDefault()
+    {
+        _volumeSlider.value = _defaultVolume;
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+    private const string _MASTER_VOLUME_KEY = "master volume";
+    private const string _DIFFICULTY_KEY = "difficulty";
+
+    private const float _MIN_VOLUME = 0f;
+    private const float _MAX_VOLUME = 1f;
+
+    public static void SetMasterVolume (float volume)
+    {
+        if (volume >= _MIN_VOLUME && volume <= _MAX_VOLUME)
+            PlayerPrefs.SetFloat(_MASTER_VOLUME_KEY, volume);
+        else
+            Debug.LogError("Master volume is out of range");
+    }
+
+    public static float GetMasterVolume()
+    {
+        return PlayerPrefs.GetFloat(_MASTER_VOLUME_KEY);
+    }
+}
