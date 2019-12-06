9c84b07f0091fbc53c88063706194dfebb05576f
Roman Saykin <sc2aru@gmail.com>
2019-12-06 22:41:40 +0300

    added cost text on defenders button

--- Assets/Scripts/DefenderButton.cs
+++ Assets/Scripts/DefenderButton.cs
@@ -1,10 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DefenderButton : MonoBehaviour
 {
     [SerializeField] private Defender _defenderPrefab;
+
+    private void Start()
+    {
+        LabelButtonWithCost();
+    }
+
+    private void LabelButtonWithCost()
+    {
+        Text costText = GetComponentInChildren<Text>();
+        if (!costText)
+            Debug.LogError(name + " has no cost text");
+        else
+            costText.text = _defenderPrefab.GetStarCost().ToString();
+    }
 
     private void OnMouseDown()
     {
